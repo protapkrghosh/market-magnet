370e45daf1c0dd4af58facaf1d6f5dd3c0d579f7
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-28 19:29:42 +0600

    some problem solved

--- src/pages/home/who-we-are/WhoWeAre.jsx
+++ src/pages/home/who-we-are/WhoWeAre.jsx
@@ -10,7 +10,7 @@ const WhoWeAre = () => {
                     <button className="btn btn-primary">Get Started</button>
                 </div>
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Box Office News</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
