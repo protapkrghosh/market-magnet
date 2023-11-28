feb92c700555484f41a5f157cd15b4122bef7180
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-28 09:26:14 +0600

    componnet lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Home from "./pages/home/home/Home";
 
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
+      <Home />
     </div>
   );
 };
---
+++ src/pages/home/advertisment/Advertisment.jsx
@@ -0,0 +1,11 @@
+
+
+const Advertisment = () => {
+    return (
+        <div>
+            <h1>this is advertisement</h1>
+        </div>
+    );
+};
+
+export default Advertisment;
