6ac55362fdd08268da9b042f637c6a647a5d7c8e
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-29 18:03:50 +0600

    news page complete

--- src/pages/home/news/News.jsx
+++ src/pages/home/news/News.jsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import { FreeMode, Pagination } from 'swiper/modules';
-
+import { FaBookmark } from "react-icons/fa";
 const News = () => {
     return (
         <div className="w-9/12 mx-auto my-11">
@@ -25,13 +25,18 @@ const News = () => {
                                 src={newse.imge}
                                 alt=""
                                 loading="lazy"
-                                className="h-96 w-full object-cover transform transition-transform hover:scale-105"
+                                className="h-96 w-full object-cover z-10 transform transition-transform scale-100 hover:scale-105"
                             />
 
-                            <div className="absolute inset-0 bg-black opacity-40"></div>
-                            <div className="absolute top-2 left-2 z-10">
-                                <h1 className="">{newse.heading}</h1>
-
+                          
+                            <div className="absolute top-2 mx-1 p-4">
+                                
+                                    <div className="bg-[#f70942] text-white text-center w-[50%]">
+                                    <p className="text-3xl">{newse.date}</p>
+                                    <p>{newse.monthe}</p>
+                                    </div>
+                                    <p className="flex items-center text-white gap-2 uppercase text-sm my-4"><FaBookmark className=""/>{newse.category}</p>
+                                    <h1 className="text-2xl font-bold  text-white">{newse.heading}</h1>
                                 <button className="btn-news">read more</button>
                             </div>
                         </div>
