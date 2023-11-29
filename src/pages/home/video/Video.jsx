787c5eb502020d8d3e15156db0ae55604601f7ba
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-29 11:14:04 +0600

    some config solved

--- src/pages/home/video/Video.jsx
+++ src/pages/home/video/Video.jsx
@@ -22,7 +22,7 @@ const Video = () => {
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>
             <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+            <p className="py-4">Press ESC key or click on ✕ button to Close</p>
           </div>
         </dialog>
        </div>
