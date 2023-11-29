696c9b85b2f4a1cb13e77fc129ac5880145c5a08
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2023-11-29 12:35:12 +0600

    our projects added

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -2,17 +2,17 @@ import { FaBuffer,FaRegStar,FaRegCheckCircle  } from "react-icons/fa";
 export  const banners =[
     {
         title:"Agency for\n outdoor \nadvertising.",
-        img:"https://i.ibb.co/R3G3nR3/banner-3.jpg",
+        img:"https://outmedia.freekytheme.com/wp-content/uploads/2023/08/slider-image3.jpg",
        
     },
     {
         title:"Build Your\nBrand image\nwith us",
-        img:"https://i.ibb.co/R3G3nR3/banner-3.jpg",
+        img:"https://outmedia.freekytheme.com/wp-content/uploads/2023/08/slider-image1.jpg",
        
     },
     {
         title:"Builds brand\nawareness\nimmediately",
-        img:"https://i.ibb.co/R3G3nR3/banner-3.jpg",
+        img:"https://outmedia.freekytheme.com/wp-content/uploads/2023/08/main-slider-3.jpg",
        
     }
   
--- src/pages/home/our-projects/OurProjects.jsx
+++ src/pages/home/our-projects/OurProjects.jsx
@@ -1,9 +1,25 @@
+import ProjectsCompononet from "../../../component/ProjectsCompononet";
 
 
 const OurProjects = () => {
+    const imge = "https://outmedia.freekytheme.com/wp-content/uploads/2023/08/bg-f.png"
+    const imgeStyle = {
+        backgroundImage: `url(${imge})`,
+        backgroundRepeat: 'no-repeat',
+
+
+    };
     return (
-        <div>
-            <h1>this is our projects</h1>
+        <div className=' w-full relative md:flex md:justify-center md:items-center py-24 md:my-96  bg-cover ' style={imgeStyle}>
+
+            <div className='text-center my-24'>
+                <p className=" my-4 text-[#f70942] tex">WHAT WE DONE</p>
+                <h1 className=" text-white text-7xl font-extrabold my-4">OUR PROJECTS.</h1>
+
+            </div>
+            <div className="absolute top-96 left-4">
+                <ProjectsCompononet />
+            </div>
         </div>
     );
 };
