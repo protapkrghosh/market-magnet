b1753cb3075fb8f64da3a04c4d9157699118eaef
Protap Ghosh <protapg518@gmail.com>
2023-11-29 10:17:48 +0600

    Create about page

--- src/pages/home/who-we-are/WhoWeAre.jsx
+++ src/pages/home/who-we-are/WhoWeAre.jsx
@@ -1,22 +1,22 @@
 
 
 const WhoWeAre = () => {
-    return (
-        <div className="hero w-9/12 mx-auto ">
-            <div className="hero-content flex-col lg:flex-row">
-                <div className="md:w-[50%]"> 
-                    <h1 className="text-3xl font-bold">WE CREATE MASS <br /> PUBLICS EYE <br /> ATTENTION</h1>
-                  
-                    <button className="btn btn-secendary">Get Started</button>
-                </div>
-                <div className="md:w-[50%]">
-                    <h1 className="text-5xl font-bold">Box Office News</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
-                </div>
-            </div>
+  return (
+    <div className="hero w-9/12 mx-auto ">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="md:w-[50%]">
+          <h1 className="text-3xl font-bold">WE CREATE MASS <br /> PUBLICS EYE <br /> ATTENTION</h1>
+
+          <button className="btn btn-secendary">Get Started</button>
         </div>
-    );
+        <div className="md:w-[50%]">
+          <h1 className="text-5xl font-bold">Box Office News</h1>
+          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhoWeAre;
